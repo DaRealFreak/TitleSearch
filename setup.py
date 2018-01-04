2aa21a9b0341d4c69cd88c38a79bb5437e19cbf0
Steffen Keuper <steffen.keuper@web.de>
2018-01-04 14:06:19 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(name='BakaUpdates',
+      version='0.0.1',
+      description='Small script to extract similar and translated titles',
+      url='https://github.com/DaRealFreak/BakaUpdates',
+      author='DaRealFreak',
+      author_email='steffen.keuper@web.de',
+      license='MIT',
+      install_requires=[
+          'numpy',
+      ],
+      zip_safe=True)
