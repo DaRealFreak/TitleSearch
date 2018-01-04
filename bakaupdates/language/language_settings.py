d6ec93e371ff5bc6653cc747b8e6f9e0ca42134e
Steffen Keuper <steffen.keuper@web.de>
2018-01-04 18:06:24 +0100

    [BUGFIX] fix range for japanese kanji

--- bakaupdates/language/language_settings.py
+++ bakaupdates/language/language_settings.py
@@ -1,5 +1,11 @@
 #!/usr/local/bin/python
 # coding: utf-8
+"""
+configuration for the different languages, mostly viable for asian languages since they have
+big differences between the used characters.
+Unicode ranges mostly taken from here: http://www.rikai.com/library/kanjitables/kanji_codes.unicode.shtml
+"""
+
 import numpy as np
 
 
@@ -32,11 +38,11 @@ class Japanese:
     JAPANESE_HIRAGANA=(0x3040, 0x5f)
     JAPANESE_KATAKANA=(0x30A0, 0x5f)
     JAPANESE_ROMAN_HALF_WIDTH_KATAKANA=(0xFF00, 0xEF)
-    JAPANESE_KANJI=(0x4e00, 0x51AF)
+    JAPANESE_KANJI=(0x4e00, 0x9FAF)
     JAPANESE_KANJI_RARE=(0x3400, 0x19BF)
     """
     UNICODE_CHARACTER_LOWERS = np.array([0x3000, 0x3040, 0x30a0, 0xff00, 0x4e00, 0x3400])
-    UNICODE_CHARACTER_UPPERS = np.array([0x303f, 0x309f, 0x30ff, 0xffef, 0x51AF, 0x4dbf])
+    UNICODE_CHARACTER_UPPERS = np.array([0x303f, 0x309f, 0x30ff, 0xffef, 0x9FAF, 0x4dbf])
 
     REQUIRES_UNICODE_CHARACTERS = True
     FORBIDS_UNICODE_CHARACTERS = False
