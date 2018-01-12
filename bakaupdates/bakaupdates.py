c3bbdd1f05c79f5622628a1d71fc490ec9a4935e
Steffen Keuper <steffen.keuper@web.de>
2018-01-12 12:18:04 +0100

    [TASK] clean found titles from keywords added by BakaUpdates

--- bakaupdates/bakaupdates.py
+++ bakaupdates/bakaupdates.py
@@ -16,6 +16,7 @@ class BakaUpdates(object):
 
     SEARCH_URL = 'https://www.mangaupdates.com/series.html'
     KNOWN_LANGUAGES = [English, Japanese, Korean]
+    ADDED_KEYWORDS = [' (Novel)']
 
     @staticmethod
     def get_similar_titles(title):
@@ -32,12 +33,21 @@ class BakaUpdates(object):
         link = requests.post(url=BakaUpdates.SEARCH_URL, params=payload)
         soup = Soup(link.text, 'html.parser')
 
+        seen_titles = []
         results = []
         for s in soup.find_all('td', attrs={"class": "text pad col1"}):
-            results.append({'title': s.text,
-                            'link': s.find_next('a', href=True)['href'],
-                            'similarity': difflib.SequenceMatcher(None, s.text.lower(), title.lower()).ratio()
-                            })
+            search_result = BakaUpdates.clean_title(s.text)
+            # I decided to add a seen titles list to prevent duplicate titles in the output.
+            # BakaUpdates search returns titles without the added keywords first, so I'll skip the second result
+            # which is most likely a novel
+            if search_result not in seen_titles:
+                results.append({'title': search_result,
+                                'link': s.find_next('a', href=True)['href'],
+                                'similarity': difflib.SequenceMatcher(None,
+                                                                      search_result.lower(),
+                                                                      title.lower()).ratio()
+                                })
+                seen_titles.append(search_result)
 
         results.sort(key=lambda item: item['similarity'], reverse=True)
         if len(results) < 1:
@@ -67,7 +77,8 @@ class BakaUpdates(object):
         alternative_titles = associated_names_tag.parent.find_next_sibling('div', attrs={'class': 'sContent'})
         for br in alternative_titles.find_all("br"):
             br.replace_with("\n")
-        alternative_titles = [title.strip() for title in alternative_titles.text.split('\n') if title.strip()]
+        alternative_titles = [BakaUpdates.clean_title(title) for title in alternative_titles.text.split('\n')
+                              if title.strip()]
 
         grouped_titles = {}
         for language in BakaUpdates.KNOWN_LANGUAGES:
@@ -76,8 +87,20 @@ class BakaUpdates(object):
 
         for title in alternative_titles:
             for language in BakaUpdates.KNOWN_LANGUAGES:
-                if matches_language(title, language):
+                if matches_language(title, language) and title not in grouped_titles[language.__name__.lower()]:
                     grouped_titles[language.__name__.lower()].append(title)
                     continue
 
         return grouped_titles
+
+    @staticmethod
+    def clean_title(title):
+        """Strip title from leftover spaces and remove keywords added by BakaUpdates
+
+        :param title:
+        :return:
+        """
+        title = title.strip()
+        for keyword in BakaUpdates.ADDED_KEYWORDS:
+            title = title.replace(keyword, '')
+        return title
