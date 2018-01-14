6856f1c0b111efd1a993d2f2da8e77a30330219e
Steffen Keuper <steffen.keuper@web.de>
2018-01-14 11:33:52 +0100

    [BUGFIX] add packages to setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup(name='BakaUpdates',
+setup(name='bakaupdates',
       version='0.0.1',
       description='Small script to extract similar and translated titles',
       url='https://github.com/DaRealFreak/BakaUpdates',
       author='DaRealFreak',
       author_email='steffen.keuper@web.de',
       license='MIT',
+      packages=find_packages(),
       install_requires=[
           'numpy', 'bs4', 'requests', 'html5lib',
       ],
