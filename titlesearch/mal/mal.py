379f5f1e577b5222815a5e2d4a90f4317ef37954
Steffen Keuper <steffen.keuper@web.de>
2018-01-24 13:34:20 +0100

    [TASK] implement get_similar_titles and get_alternative_titles for MyAnimeList

--- titlesearch/mal/mal.py
+++ titlesearch/mal/mal.py
@@ -1,5 +1,9 @@
 #!/usr/local/bin/python
 # coding: utf-8
+import re
+
+import bs4
+import jellyfish
 import requests
 from bs4 import BeautifulSoup as Soup
 
@@ -9,9 +13,14 @@ from titlesearch.language.language_settings import *
 class MyAnimeList(object):
     """Module for extracting alternative language titles for titles from mangaupdates.com"""
 
-    SEARCH_URL = 'https://vndb.org/v/all'
-    KNOWN_LANGUAGES = [English, Japanese, Korean]
+    SEARCH_URL = 'https://myanimelist.net/search/all'
+    KNOWN_LANGUAGES = [English, Japanese]
     ADDED_KEYWORDS = [' (Novel)']
+    MAPPING = {
+        'English:': 'english',
+        'Synonyms:': 'english',
+        'Japanese:': 'japanese'
+    }
 
     @staticmethod
     def get_similar_titles(title):
@@ -21,11 +30,24 @@ class MyAnimeList(object):
         :return:
         """
         payload = {
-            'sq': title
+            'q': title
         }
 
+        results = []
+
         link = requests.get(url=MyAnimeList.SEARCH_URL, params=payload)
-        soup = Soup(link.text, 'html.parser')
+        # html5lib parser since html.parser will fail at the content-left div already
+        soup = Soup(link.text, 'html5lib')
+        for search_result in soup.select('div.content-left div.list.di-t.w100 a.hoverinfo_trigger'):
+            search_group = re.search('/anime/|/manga/', search_result['href'])
+            if search_result.text.strip() and search_group:
+                results.append({
+                    'title': search_result.text.strip(),
+                    'link': search_result['href'],
+                    'similarity': jellyfish.jaro_distance(search_result.text.strip().lower(), title.lower())
+                })
+        results.sort(key=lambda item: item['similarity'], reverse=True)
+        return results
 
     @staticmethod
     def get_alternative_titles(title='', link=''):
@@ -35,23 +57,64 @@ class MyAnimeList(object):
         :param link:
         :return:
         """
+        grouped_titles = {}
+        for language in MyAnimeList.KNOWN_LANGUAGES:
+            grouped_titles[language.__name__.lower()] = []
+
         if title and not link:
-            link = MyAnimeList.get_similar_titles(title)[0]['link']
+            link = MyAnimeList.get_similar_titles(title)
+            if link:
+                link = link[0]['link']
+            else:
+                return grouped_titles
 
         link = requests.get(url=link)
 
-        # html.parser can't handle <br> tags instead of <br/> tags and will append all titles as child
-        # to the previous title, html5lib is slower but works properly
         soup = Soup(link.text, 'html5lib')
 
-    @staticmethod
-    def clean_title(title):
-        """Strip title from leftover spaces and remove keywords added by VisualNovelDatabase
-
-        :param title:
-        :return:
+        release_title = soup.find('span', attrs={'itemprop': 'name'})
         """
-        title = title.strip()
-        for keyword in MyAnimeList.ADDED_KEYWORDS:
-            title = title.replace(keyword, '')
-        return title
+        recreating an array vs appending values to an array
+
+        a = []; a = [1]
+        runtime:
+            >> timeit.timeit('a = []; a = [1]', number=10**7)
+            >> 0.5276432445431638
+
+        1            0 BUILD_LIST               0
+                     2 STORE_NAME               0 (a)
+                     4 LOAD_CONST               0 (1)
+                     6 BUILD_LIST               1
+                     8 STORE_NAME               0 (a)
+                     10 LOAD_CONST              1 (None)
+                     12 RETURN_VALUE
+
+        a = []; a.append(1)
+        runtime:
+            >> timeit.timeit('a = []; a.append(1)', number=10**7)
+            >> 0.99629117289982
+
+        1           0 BUILD_LIST               0
+                    2 STORE_NAME               0 (a)
+                    4 LOAD_NAME                0 (a)
+                    6 LOAD_ATTR                1 (append)
+                    8 LOAD_CONST               0 (1)
+                    10 CALL_FUNCTION           1
+                    12 POP_TOP
+                    14 LOAD_CONST              1 (None)
+                    16 RETURN_VALUE
+        """
+        grouped_titles['english'] = [release_title.text]
+
+        for search_result in soup.find_all('div', attrs={'class': 'spaceit_pad'}):  # type: bs4.element.Tag
+            category = search_result.find('span', attrs={'class': 'dark_text'})
+            if category:
+                value = "".join([t for t in search_result.contents if type(t) == bs4.element.NavigableString]).strip()
+                if category.text.strip() == 'Synonyms:':
+                    for synonym in value.split(', '):
+                        grouped_titles[MyAnimeList.MAPPING[category.text]].append(synonym)
+                else:
+                    if category.text.strip() in MyAnimeList.MAPPING:
+                        grouped_titles[MyAnimeList.MAPPING[category.text.strip()]].append(value)
+
+        return grouped_titles
