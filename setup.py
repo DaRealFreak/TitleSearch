5f49e37dbfddf72d96477fa821da86880483381e
Steffen Keuper <steffen.keuper@web.de>
2018-01-24 12:02:19 +0100

    [TASK] add jellyfish as requirement

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ setup(name='titlesearch',
       version='0.0.1',
       description='Small script to extract similar and translated titles from '
                   'light novels, novels, visual novels, mangas and animes',
-      url='https://github.com/DaRealFreak/BakaUpdates',
+      url='https://github.com/DaRealFreak/TitleSearch',
       author='DaRealFreak',
       author_email='steffen.keuper@web.de',
       license='MIT',
       packages=find_packages(),
       install_requires=[
-          'numpy', 'bs4', 'requests', 'html5lib',
+          'numpy', 'bs4', 'requests', 'html5lib', 'jellyfish'
       ],
       zip_safe=True)
