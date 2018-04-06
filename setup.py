9c601e1d9a1b810231d7762c99bcbef7c9828f4a
Steffen Keuper <steffen.keuper@web.de>
2018-04-06 23:31:48 +0200

    [TASK] specify dependency versions, extract setup details to __version__.py

--- setup.py
+++ setup.py
@@ -1,17 +1,33 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+
+import os
+
 from setuptools import setup, find_packages
 
-setup(name='titlesearch',
-      version='0.0.1',
-      description='Small script to extract similar and translated titles from '
-                  'light novels, novels, visual novels, mangas and animes',
-      url='https://github.com/DaRealFreak/TitleSearch',
-      author='DaRealFreak',
-      author_email='steffen.keuper@web.de',
-      license='MIT',
+current_directory = os.path.abspath(os.path.dirname(__file__))
+
+about = {}
+with open(os.path.join(current_directory, 'titlesearch', '__version__.py'), 'r') as f:
+    exec(f.read(), about)
+
+setup(name=about['__title__'],
+      version=about['__version__'],
+      description=about['__description__'],
+      url=about['__url__'],
+      author=about['__author__'],
+      author_email=about['__author_email__'],
+      license=about['__license__'],
       packages=find_packages(),
       install_requires=[
-          'numpy', 'bs4', 'requests', 'html5lib', 'jellyfish'
+          'bs4>=0.0.1',
+          'numpy>=1.14.2',
+          'jellyfish>=0.5.6',
+          'requests>=2.18.4',
+          'html5lib>=1.0.1'
       ],
+      extras_require={
+          'unittests': [
+          ]
+      },
       zip_safe=True)
---
+++ titlesearch/__version__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+__title__ = 'titlesearch'
+__version__ = '0.0.1'
+__description__ = 'Small script to extract similar and translated titles from ' \
+                  'light novels, novels, visual novels, mangas and animes'
+__url__ = 'https://github.com/DaRealFreak/TitleSearch'
+__author__ = 'DaRealFreak'
+__author_email__ = 'steffen.keuper@web.de'
+__license__ = 'MIT'
